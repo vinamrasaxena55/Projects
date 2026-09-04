7dab91e4ca5e76e928223bba1e3d88447374e8a7
vinamra887 <vinamra.240101121@iiitbh.ac.in>
2026-09-05 00:55:03 +0530

    Updated Router and HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {Routes,Route} from 'react-router';
+import { HomePage } from './pages/HomePage';
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
+//Routes for combing html done in this and main.jsx
+//rout=page, route basically adds page to website
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Routes>
+    <Route index element={<HomePage />} />
+    <Route path="checkout" element={<div>Test Checkout Page</div>} />
+    </Routes>
+   
   )
 }
-
+// <Route path="/" element={<HomePage />} 
+//path="/"  ==>index
+//'/' means empty url and what to display on Path is HomePage
+//path="checkout" then checkout page displayed
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {BrowserRouter} from 'react-router';//from node modules ,this helps in combining html pages and run them as 1
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+       <App />
+    </BrowserRouter>
+    
   </StrictMode>,
 )
